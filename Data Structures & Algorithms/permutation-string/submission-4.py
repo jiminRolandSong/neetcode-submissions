3e9095372fb3bbfb3b4d2b23d3814df31b927401
jiminRolandSong <124591008+jiminRolandSong@users.noreply.github.com>
2026-05-27 14:29:15 -0400

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        start = 0
+
+        freq1 = defaultdict(int)
+
+        for i in s1:
+            freq1[i] += 1
+        
+        
+        for s in range(len(s2) - len(s1) + 1):
+            cur_window = s2[s: s + len(s1)]
+            check = True
+            for key, value in freq1.items():
+
+                if cur_window.count(key) != value:                   
+                    check = False
+            if check:
+                return True
+
+        
+        return False
+        
