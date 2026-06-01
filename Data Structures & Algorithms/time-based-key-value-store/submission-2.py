42864b4db8f31e77f7f1048257972a7a917da827
jiminRolandSong <124591008+jiminRolandSong@users.noreply.github.com>
2026-06-01 16:17:44 -0400

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.py
@@ -0,0 +1,34 @@
+class TimeMap:
+
+    def __init__(self):
+        self.emos = {}
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.emos:
+            self.emos[key] = []
+
+        self.emos[key].append([value, timestamp])
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+
+        emotion = ""
+        values = self.emos.get(key, [])
+
+        left = 0
+        right = len(values) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            time = values[mid][1]
+
+            if values[mid][1] <= timestamp:
+                emotion = values[mid][0]
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return emotion
+
+        
