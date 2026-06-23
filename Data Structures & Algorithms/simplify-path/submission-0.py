93ad8a48a4f7333dd592603a1dbf0964f62b4b89
jiminRolandSong <124591008+jiminRolandSong@users.noreply.github.com>
2026-06-23 17:59:31 -0400

    Add: simplify-path - submission-0

---
+++ Data Structures & Algorithms/simplify-path/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        splited = path.split('/')
+        print(splited)
+
+        okays = []
+        for i in splited:
+            current = i
+
+            if len(current) == 0:
+                continue
+
+            if len(okays) > 0 and current == "..":
+                okays.pop()
+            elif current != '.' and current != "..":
+                okays.append(current)
+        
+        return '/' + '/'.join(okays)
